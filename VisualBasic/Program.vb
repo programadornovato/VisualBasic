b2102cdfcfb2eb330e62addd916a4d09e96b987c
Programador Novato <info@programadornovato.com>
2021-06-27 15:33:28 -0500

    Comentarios en Visual Basic.NET [05] Video: https://youtu.be/pZXFXjVwix4

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -1,12 +1,17 @@
 Imports System
 
+'Autor: Programador Novato
+'Fecha: 01/01/2021
+'Este modulo sirve para enseñar como funciona Visual Basic.NET
 Module Program
+    'Este codigo escribe un texto en amarillo con fondo azul
     Sub Main(args As String())
+        REM Este es otro comentario
         Console.ForegroundColor = ConsoleColor.Yellow
         Console.BackgroundColor = ConsoleColor.Blue
         Console.Clear()
         Console.WriteLine("Hola mundo!")
-        Console.ForegroundColor = ConsoleColor.Green
+        'Console.ForegroundColor = ConsoleColor.Green
         Console.WriteLine("Hola novato")
         Console.Read()
     End Sub
