c424b6c81eee4ac3973fc148ca083d390ccba3a9
Programador Novato <info@programadornovato.com>
2021-06-26 19:29:03 -0500

    Agregar archivos de proyecto.

---
+++ VisualBasic/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.BackgroundColor = ConsoleColor.Blue
+        Console.Clear()
+        Console.WriteLine("Hola mundo!")
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Hola novato")
+        Console.Read()
+    End Sub
+End Module
