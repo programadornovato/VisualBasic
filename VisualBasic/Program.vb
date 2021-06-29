2aae3dc555f24bfbf0e203d123df20306ef36f6a
Programador Novato <info@programadornovato.com>
2021-06-29 18:47:50 -0500

    Operadores lógicos en Visual Basic.NET [11] Video: https://youtu.be/StQNsj2fRoU

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,22 +6,19 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Sub Main(args As String())
-        Dim numString1 = "10.22"
-        Dim numString2 = "20.15"
-        'Dim numInt1 = Integer.Parse(numString1)
-        'Dim numInt2 = Integer.Parse(numString2)
-        'Dim resultado = numInt1 + numInt2
+        'Dim a = False
+        'Dim b = False
+        'Dim res = a Or b
+        'Console.WriteLine(res)
 
-        'Dim numFloat1 = Single.Parse(numString1)
-        'Dim numFloat2 = Single.Parse(numString2)
-        'Dim resultado = numFloat1 + numFloat1
+        'Dim a = False
+        'Dim b = False
+        'Dim res = a And b
+        'Console.WriteLine(res)
 
-        Dim numDoble1 = Single.Parse(numString1)
-        Dim numDoble2 = Single.Parse(numString2)
-        Dim resultado = numDoble1 + numDoble1
-
-        Console.WriteLine(resultado)
-
+        Dim a = False
+        Dim res = Not a
+        Console.WriteLine(res)
 
         Console.Read()
     End Sub
