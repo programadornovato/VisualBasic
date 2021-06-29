73940ff75af4c6a933c56e739af27f8a6c88ef9e
Programador Novato <info@programadornovato.com>
2021-06-29 16:41:30 -0500

    Parse (Cambio) de tipos de datos en Visual Basic.NET [10] Video: https://youtu.be/hcGUUrMDuhc

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,12 +6,23 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Sub Main(args As String())
-        Dim miPagina = "ProgramadorNovato.com"
-        'miPagina = miPagina.ToUpper()
-        'miPagina = miPagina.ToLower()
-        miPagina = miPagina.Replace(".com", ".mx")
-        Dim miPresentacion = "Esta es mi pagina web: "
-        Console.WriteLine(miPresentacion & miPagina)
+        Dim numString1 = "10.22"
+        Dim numString2 = "20.15"
+        'Dim numInt1 = Integer.Parse(numString1)
+        'Dim numInt2 = Integer.Parse(numString2)
+        'Dim resultado = numInt1 + numInt2
+
+        'Dim numFloat1 = Single.Parse(numString1)
+        'Dim numFloat2 = Single.Parse(numString2)
+        'Dim resultado = numFloat1 + numFloat1
+
+        Dim numDoble1 = Single.Parse(numString1)
+        Dim numDoble2 = Single.Parse(numString2)
+        Dim resultado = numDoble1 + numDoble1
+
+        Console.WriteLine(resultado)
+
+
         Console.Read()
     End Sub
 End Module
