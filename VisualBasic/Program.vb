628437b4d21f4777d355cba8f0896f53a6c75737
Programador Novato <info@programadornovato.com>
2021-06-29 03:16:36 -0500

    (Ejercicio) Tipos de datos en C# [07] Video: https://youtu.be/2Qpc41KIm80

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,13 +6,21 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Sub Main(args As String())
-        REM Este es otro comentario
-        Console.ForegroundColor = ConsoleColor.Yellow
-        Console.BackgroundColor = ConsoleColor.Blue
-        Console.Clear()
-        Console.WriteLine("Hola mundo!")
-        'Console.ForegroundColor = ConsoleColor.Green
-        Console.WriteLine("Hola novato")
+        Dim num1 As Integer = 1
+        Dim num2 As Integer = 1
+        Dim suma As Integer = num1 + num2
+        Dim num3 As Integer = 1, num4 As Integer = 2, num5 As Integer = 3
+        Dim valD1 As Double = 5.5
+        Dim valD2 As Double = 6.6
+        Dim sumaD As Double = valD1 + valD2
+        Dim valFloat As Single = 5.5F
+        Dim sumaFD = sumaD + valFloat
+
+        Console.WriteLine(num1 & "+" & num2 & " =" & suma)
+        Console.WriteLine(valD1 & "+" & valD2 & " =" & sumaD)
+        Console.WriteLine("suma es de tipo " & suma.GetType.ToString())
+        Console.WriteLine("sumaFD es del tipo " & sumaFD.GetType.ToString())
+
         Console.Read()
     End Sub
 End Module
