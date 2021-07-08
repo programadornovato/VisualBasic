027f2d83a178b315846d174389e9a0e5aef7df45
Programador Novato <info@programadornovato.com>
2021-07-07 19:23:26 -0500

    Ingresar datos por consola en Visual Basic.NET [19]⛓️ Video: https://youtu.be/WIHOx2kJjWY

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,23 +6,13 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Console.WriteLine("Math.PI=" & Math.PI)
-        Console.WriteLine("Math.E=" & Math.E)
-        Console.WriteLine("Math.Min(5.5,3.5)=" & Math.Min(5.5, 3.5))
-        Console.WriteLine("Math.Max(5.5,3.5)=" & Math.Max(5.5, 3.5))
-        Console.WriteLine("Math.Round(5.5)=" & Math.Round(5.5))
-        Console.WriteLine("Math.Round(-5.5)=" & Math.Round(-5.5))
-        Console.WriteLine("Math.Ceiling(5.5)=" & Math.Ceiling(5.5))
-        Console.WriteLine("Math.Ceiling(-5.5)=" & Math.Ceiling(-5.5))
-        Console.WriteLine("Math.Floor(5.5)=" & Math.Floor(5.5))
-        Console.WriteLine("Math.Floor(-5.5)=" & Math.Floor(-5.5))
-        Console.WriteLine("Math.Truncate(-5.5)=" & Math.Truncate(-5.5))
-        Console.WriteLine("Math.Abs(-5.5)=" & Math.Abs(-5.5))
-        Console.WriteLine("Math.Abs(5.5)=" & Math.Abs(5.5))
-        Console.WriteLine("Math.Sign(-5.5)=" & Math.Sign(-5.5))
-        Console.WriteLine("Math.Sign(5.5)=" & Math.Sign(5.5))
-        Console.WriteLine("Math.Pow(3,3)=" & Math.Pow(3, 3))
-        Console.WriteLine("Math.Sqrt(9)=" & Math.Sqrt(9))
-        Console.Read()
+        Console.WriteLine("¿Humano cual es tu nombre?")
+        Dim nombre = Console.ReadLine()
+        Console.WriteLine("¿" & nombre & " en que año naciste?")
+        Dim añoNacimiento = Integer.Parse(Console.ReadLine())
+        Dim edad = DateTime.Now.Year - añoNacimiento
+        Console.WriteLine("¿Humano cuanto dinero ganas?")
+        Dim sueldo = Single.Parse(Console.ReadLine())
+        Console.WriteLine("Pobre " & nombre & " a tus " & edad & " años solo ganas " & sueldo & " pesitos")
     End Sub
 End Module
