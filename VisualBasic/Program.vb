99394f4b63729961d4194706628612d49e43ddd5
Programador Novato <info@programadornovato.com>
2021-07-07 19:57:04 -0500

    Condicionales en Visual Basic.NET (If, ElseIf y Else)[20]⛓️ video: https://youtu.be/rxn9KEc0ZRo

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,13 +6,18 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Console.WriteLine("¿Humano cual es tu nombre?")
-        Dim nombre = Console.ReadLine()
-        Console.WriteLine("¿" & nombre & " en que año naciste?")
-        Dim añoNacimiento = Integer.Parse(Console.ReadLine())
-        Dim edad = DateTime.Now.Year - añoNacimiento
-        Console.WriteLine("¿Humano cuanto dinero ganas?")
-        Dim sueldo = Single.Parse(Console.ReadLine())
-        Console.WriteLine("Pobre " & nombre & " a tus " & edad & " años solo ganas " & sueldo & " pesitos")
+        Console.WriteLine("Humano, por favor, ingresa un numero entre 0 y 10")
+        Dim cal = Double.Parse(Console.ReadLine())
+        Dim calRedondeada = Convert.ToInt32(Math.Round(cal))
+        If (calRedondeada >= 0) AndAlso (calRedondeada < 6) Then
+            Console.WriteLine("El alumno se chingo")
+        ElseIf calRedondeada >= 6 AndAlso calRedondeada <= 9 Then
+            Console.WriteLine("El alumno YA chingo")
+        ElseIf calRedondeada = 10 Then
+            Console.WriteLine("El alumno es una v3rg4")
+        Else
+            Console.WriteLine("Humano estupido te pedi un numero entre 0 y 10 y me entragas un " & calRedondeada)
+        End If
+        Console.Read()
     End Sub
 End Module
