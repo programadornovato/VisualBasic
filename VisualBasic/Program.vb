935545ae3f3ef6afdd8fbfe07742163a3225d40a
Programador Novato <info@programadornovato.com>
2021-07-08 21:30:43 -0500

    Ejercicio 03.- Calcular el volumen de un vaso en Visual Basic.NET [23]⛓️ Video: https://youtu.be/5x_7gWrnnOU

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -7,15 +7,27 @@ Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
         Try
-            Console.WriteLine("Por favor humano ingresa un NUMERO:")
-            Dim numeroS = Console.ReadLine()
-            Dim numeroI = Integer.Parse(numeroS)
-            Dim cuadrado = numeroI * numeroI
-            Console.WriteLine("Humano el numero " & numeroI & " al cuadrado es:" & cuadrado)
+            Console.WriteLine("¿Humano con cuantos litros de cerveza te emborrachas?")
+            Dim litros = Integer.Parse(Console.ReadLine())
+            Dim mililitros = litros * 1000
+            Console.WriteLine("¿Humano cual es el diametro de tu vaso?")
+            Dim D = Integer.Parse(Console.ReadLine())
+            Console.WriteLine("¿Humano cual es la altura de tu vaso?")
+            Dim A = Integer.Parse(Console.ReadLine())
+            Dim R = D / 2
+            Dim V = Math.PI * R * 2 * A
+            Dim vasosLimite = mililitros / V
+
+            'Console.WriteLine("V=" & V & "  vasosLimite=" & vasosLimite)
+            Console.WriteLine("¿Humano cunatos vasos de cerveza has tomado?")
+            Dim vasosTomados = Integer.Parse(Console.ReadLine())
+            If vasosTomados <= vasosLimite Then
+                Console.WriteLine("Humano te hacen falta " & vasosLimite - vasosTomados & " vasos para estar borracho")
+            Else
+                Console.WriteLine("Humano ya te exediste con " & vasosTomados - vasosLimite & " vasos YA ESTAS BORRACHO")
+            End If
         Catch ex As Exception
-            Console.WriteLine("Humano estupido te pedi un NUMERO " & ex.ToString())
-        Finally
-            Console.WriteLine("Se ejecuto Finally")
+            Console.WriteLine("¡¡¡HUMANO YA ESTAS TAN BORRACHO QUE NI PUEDES ESCRIBIR UN NUMERO!!!")
         End Try
 
         Console.Read()
