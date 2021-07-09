83bf64e6d36aad6edc3deff9ec07dc86a9de77ea
Programador Novato <info@programadornovato.com>
2021-07-09 17:08:45 -0500

    Operadores de asignación en Visual Basic.NET[25]⛓️ Video: https://youtu.be/5aFZ-Iw4ryo

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,42 +6,29 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Try
-            Console.WriteLine("Hola humano, bienvenido al banco pn")
-            Console.WriteLine("Selecciona alguna de las siguientes opciones")
-            Console.WriteLine("1.- Ver saldo")
-            Console.WriteLine("2.- Ingresar dinero")
-            Console.WriteLine("3.- Sacar dinero")
-            Console.WriteLine("4.- Salir")
-            Dim opcion = Integer.Parse(Console.ReadLine())
-            Dim saldoInicial = 100.0
-            Select Case opcion
-                Case 1
-                    Console.WriteLine("Humano tu saldo es de " & saldoInicial)
-                Case 2
-                    Console.WriteLine("Humano cuanto dinero quieres ingresar")
-                    Dim ingresar = Double.Parse(Console.ReadLine())
-                    saldoInicial = saldoInicial + ingresar
-                    Console.WriteLine("Humano haz ingresado " & ingresar & " tu saldo es de " & saldoInicial)
-                Case 3
-                    Console.WriteLine("Humano cuanto dinero quieres sacar")
-                    Dim sacar = Double.Parse(Console.ReadLine())
-                    If sacar > saldoInicial Then
-                        Console.WriteLine("Humano pillin no cuentas con saldo suficiente")
-                    Else
-                        saldoInicial = saldoInicial - sacar
-                        Console.WriteLine("Humano haz sacado " & sacar & " tu saldo es de " & saldoInicial)
-                    End If
-                Case 4
-                    Console.WriteLine("Adios humano gracias por usr el pn")
-                Case Else
-                    Console.WriteLine("Humano estupido te pedi que seleccionar alguna de las opciones")
+        Dim a = 6.0
+        Dim b = 5.0
+        Console.WriteLine("a=" & a & " b=" & b)
 
-            End Select
+        ''a = a + b
+        'a += b
+        'Console.WriteLine("a=" & a & " b=" & b)
 
-        Catch ex As Exception
-            Console.WriteLine("Humano te pedi un numero valido " & ex.ToString)
-        End Try
+        ''a = a - b
+        'a -= b
+        'Console.WriteLine("a=" & a & " b=" & b)
+
+        ''a = a * b
+        'a *= b
+        'Console.WriteLine("a=" & a & " b=" & b)
+
+        ''a = a / b
+        'a /= b
+        'Console.WriteLine("a=" & a & " b=" & b)
+
+        'a = a mod b
+        a = a Mod b
+        Console.WriteLine("a=" & a & " b=" & b)
 
         Console.Read()
     End Sub
