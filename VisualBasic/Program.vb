48dc8bb98e60eddca1511a81b22b1d45deaf3ed4
Programador Novato <info@programadornovato.com>
2021-07-09 18:17:22 -0500

    Ejercicio 05.- Redondear el cambio en Visual Basic.NET[26]⛓️ Video: https://youtu.be/UGCl_wigTXc

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,29 +6,29 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim a = 6.0
-        Dim b = 5.0
-        Console.WriteLine("a=" & a & " b=" & b)
-
-        ''a = a + b
-        'a += b
-        'Console.WriteLine("a=" & a & " b=" & b)
-
-        ''a = a - b
-        'a -= b
-        'Console.WriteLine("a=" & a & " b=" & b)
-
-        ''a = a * b
-        'a *= b
-        'Console.WriteLine("a=" & a & " b=" & b)
-
-        ''a = a / b
-        'a /= b
-        'Console.WriteLine("a=" & a & " b=" & b)
-
-        'a = a mod b
-        a = a Mod b
-        Console.WriteLine("a=" & a & " b=" & b)
+        Try
+            Dim p1 = 0.0
+            Dim p2 = 0.0
+            Dim p3 = 0.0
+            Console.WriteLine("Cajero ingresa el valor del producto 1")
+            p1 = Double.Parse(Console.ReadLine())
+            Console.WriteLine("Cajero ingresa el valor del producto 2")
+            p2 = Double.Parse(Console.ReadLine())
+            Console.WriteLine("Cajero ingresa el valor del producto 3")
+            p3 = Double.Parse(Console.ReadLine())
+            Dim suma As Double = p1 + p2 + p3
+            Console.WriteLine("Humano deseas redondear tus centavos (S/N)")
+            Dim res = Console.ReadLine()
+            If res = "S" Or res = "s" Then
+                Dim sumaRedondeada As Double = Math.Ceiling(suma)
+                Dim redondeo As Double = sumaRedondeada - suma
+                Console.WriteLine("Muchas gracias humano por redondear " & String.Format("{0:C}", redondeo))
+            Else
+                Console.WriteLine("Pinche humano micerable")
+            End If
+        Catch ex As Exception
+            Console.WriteLine("Cajero estupido escribiste un valor no debido " & ex.ToString)
+        End Try
 
         Console.Read()
     End Sub
