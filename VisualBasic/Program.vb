030cfd50d7c8826e66aa97bb4d05cd2a4b54f953
Programador Novato <info@programadornovato.com>
2021-07-10 03:43:14 -0500

    Ejercicio 06.- Obtener edad a partir de su fecha de nacimiento en Visual Basic.NET[27]⛓️ Video: https://youtu.be/6JCqrIvYt4s

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,30 +6,19 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
+        Console.WriteLine("Humano cual es tu fecha de nacimiento en formato dd/MM/yyyy")
+        Dim fechaNacimientoString = Console.ReadLine()
+        'Dim fechaNacimientoDate As DateTime
         Try
-            Dim p1 = 0.0
-            Dim p2 = 0.0
-            Dim p3 = 0.0
-            Console.WriteLine("Cajero ingresa el valor del producto 1")
-            p1 = Double.Parse(Console.ReadLine())
-            Console.WriteLine("Cajero ingresa el valor del producto 2")
-            p2 = Double.Parse(Console.ReadLine())
-            Console.WriteLine("Cajero ingresa el valor del producto 3")
-            p3 = Double.Parse(Console.ReadLine())
-            Dim suma As Double = p1 + p2 + p3
-            Console.WriteLine("Humano deseas redondear tus centavos (S/N)")
-            Dim res = Console.ReadLine()
-            If res = "S" Or res = "s" Then
-                Dim sumaRedondeada As Double = Math.Ceiling(suma)
-                Dim redondeo As Double = sumaRedondeada - suma
-                Console.WriteLine("Muchas gracias humano por redondear " & String.Format("{0:C}", redondeo))
-            Else
-                Console.WriteLine("Pinche humano micerable")
-            End If
+            Dim fechaNacimientoDate = DateTime.ParseExact(fechaNacimientoString, "dd/MM/yyyy", Nothing)
+            Dim fechaHoy = DateTime.Now
+            Dim diferencia = fechaHoy - fechaNacimientoDate
+            Dim dias = diferencia.TotalDays
+            Dim años = Math.Floor(dias / 365)
+            Console.WriteLine("Humano tu tienes " & años & " años")
         Catch ex As Exception
-            Console.WriteLine("Cajero estupido escribiste un valor no debido " & ex.ToString)
+            Console.WriteLine("Humano estupido te pedi una fecha en formato dd/MM/yyyy")
         End Try
-
         Console.Read()
     End Sub
 End Module
