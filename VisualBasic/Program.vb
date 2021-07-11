f8bd5d601b40df0b56d1e032c97ba9bb0f05d2d5
Programador Novato <info@programadornovato.com>
2021-07-11 02:31:43 -0500

    Random en Visual Basic.NET[30]⛓️ Video: https://youtu.be/xjklZi02vMI

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,26 +6,25 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim entro = False
-        Console.WriteLine("Humano ingresa un numero entre 1 y 999:")
-        Dim num = Integer.Parse(Console.ReadLine())
-        If num >= 1 And num <= 9 Then
-            Console.WriteLine("Humano tu numero " & num & " es de tipo unidad")
-            entro = True
-        Else
-            If num >= 10 And num <= 99 Then
-                Console.WriteLine("Humano tu numero " & num & " es de tipo decena")
-                entro = True
-            Else
-                If num >= 100 And num <= 999 Then
-                    Console.WriteLine("Humano tu numero " & num & " es de tipo centena")
-                    entro = True
-                End If
-            End If
-        End If
-        If entro = False Then
-            Console.WriteLine("Humano estupido tu numero no esta en el rango de 1 y 999")
-        End If
+        Dim rand = New Random()
+
+        Dim numEntero = rand.Next
+        Console.WriteLine("numEntero=" & numEntero & " " & numEntero.GetType.ToString)
+
+        Dim numEnteroConUnParametro = rand.Next(7)
+        Console.WriteLine("numEnteroConUnParametro=" & numEnteroConUnParametro & " " & numEnteroConUnParametro.GetType.ToString)
+
+        Dim numEnteroConDosParametro = rand.Next(1, 7)
+        Console.WriteLine("numEnteroConDosParametro=" & numEnteroConDosParametro & " " & numEnteroConDosParametro.GetType.ToString)
+
+        Dim numeroDouble = rand.NextDouble
+        Console.WriteLine("numeroDouble=" & numeroDouble & " " & numeroDouble.GetType.ToString)
+
+
+        Dim numDoubleConDosParametro = rand.Next(1, 9) / 10
+        Console.WriteLine("numDoubleConDosParametro=" & numDoubleConDosParametro & " " & numDoubleConDosParametro.GetType.ToString)
+
+
         Console.Read()
     End Sub
 End Module
