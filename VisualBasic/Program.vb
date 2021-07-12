7dbd96332121459056eaa3e95784414a7eae3eba
Programador Novato <info@programadornovato.com>
2021-07-12 05:44:24 -0500

    Ciclo do while en Visual Basic.NET [33]⛓️ Video: https://youtu.be/e54QS46B6ZM

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,24 +6,26 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-
-        'Dim i = 1
-        'While i <= 10
+        'Dim i = 11
+        'Do
         '    Console.WriteLine("i=" & i)
         '    i = i + 1
-        'End While
-        'Dim i = 10
-        'While i >= 1
+        'Loop While i <= 10
+
+        'Dim i = -10
+        'Do
         '    Console.WriteLine("i=" & i)
         '    i = i - 1
-        'End While
-        Console.WriteLine("Humano por favor ingresa un numero de veces a repetir")
-        Dim repetir = Integer.Parse(Console.ReadLine())
-        Dim i = 1
-        While i <= repetir
+        'Loop While i >= 1
+
+        Dim rand = New Random()
+        Dim aleatorio = rand.Next(1, 7)
+        Console.WriteLine("aleatorio=" & aleatorio)
+        Dim i = 10
+        Do
             Console.WriteLine("i=" & i)
             i = i + 1
-        End While
+        Loop While i <= aleatorio
         Console.Read()
     End Sub
 End Module
