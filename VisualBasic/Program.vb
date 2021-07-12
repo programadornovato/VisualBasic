b8b57dca7f8edda8829159159439b3fdffc8ff9b
Programador Novato <info@programadornovato.com>
2021-07-12 04:51:27 -0500

    Ciclo do while en Visual Basic.NET [33]⛓️ Video: https://youtu.be/y0KE5tyzSfM

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,26 +6,24 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim rand = New Random()
-        Dim d1 = rand.Next(1, 7)
-        Dim d2 = rand.Next(1, 7)
-        Dim d3 = rand.Next(1, 7)
-        Console.WriteLine("d1=" & d1)
-        Console.WriteLine("d2=" & d2)
-        Console.WriteLine("d3=" & d3)
-        If d1 = 6 And d2 = 6 And d3 = 6 Then
-            Console.WriteLine("Excelnte")
-        Else
-            If (d1 = 6 And d2 = 6) Or (d1 = 6 And d3 = 6) Or (d2 = 6 And d3 = 6) Then
-                Console.WriteLine("Muy bien")
-            Else
-                If d1 = 6 Or d2 = 6 Or d3 = 6 Then
-                    Console.WriteLine("Regular")
-                Else
-                    Console.WriteLine("Pesimo")
-                End If
-            End If
-        End If
+
+        'Dim i = 1
+        'While i <= 10
+        '    Console.WriteLine("i=" & i)
+        '    i = i + 1
+        'End While
+        'Dim i = 10
+        'While i >= 1
+        '    Console.WriteLine("i=" & i)
+        '    i = i - 1
+        'End While
+        Console.WriteLine("Humano por favor ingresa un numero de veces a repetir")
+        Dim repetir = Integer.Parse(Console.ReadLine())
+        Dim i = 1
+        While i <= repetir
+            Console.WriteLine("i=" & i)
+            i = i + 1
+        End While
         Console.Read()
     End Sub
 End Module
