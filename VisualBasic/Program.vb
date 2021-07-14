96556b099db3a2d24a1c0c542a816edf76121ad2
Programador Novato <info@programadornovato.com>
2021-07-14 06:32:11 -0500

    Arreglos en Visual Basic.NET[38]⛓️ Video: https://youtu.be/wy7b353gV4E

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,13 +6,11 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Console.WriteLine("Humano que altura va atener tu escalera")
-        Dim altura = Integer.Parse(Console.ReadLine)
-        For i = 1 To altura
-            For j = 1 To i
-                Console.Write("*")
-            Next
-            Console.WriteLine()
+        Dim arreglo = New Integer() {5, 8, 3, 7, 4, 1}
+        Console.WriteLine("El elemento en la posicion 4 es " & arreglo(3))
+        Console.WriteLine("El tamaño es " & arreglo.Length)
+        For i = 0 To arreglo.Length - 1
+            Console.WriteLine("El valor del elemento " & i + 1 & " es " & arreglo(i))
         Next
         Console.Read()
     End Sub
