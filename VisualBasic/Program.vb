ace5aa6970674d39537dece8bc91f1209470fa82
Programador Novato <info@programadornovato.com>
2021-07-14 04:11:06 -0500

    Ejercicio 10.- Dibujar una escalera con asteriscos con ciclo for en Visual Basic.NET[36]⛓️ Video: https://youtu.be/I8FLkWI4RUA

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,31 +6,14 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        'For i = 1 To 5 Step 2
-        '    Console.WriteLine("i=" & i)
-        'Next
-
-        'Dim i = 1
-        'While i <= 5
-        '    Console.WriteLine("i=" & i)
-        '    i = i + 2
-        'End While
-
-        'For i = 5 To 1 Step -2
-        '    Console.WriteLine("i=" & i)
-        'Next
-
-        Console.WriteLine("Humano ingresa el numero de repeticiones")
-        Dim repeticion As Integer
-        Try
-            repeticion = Integer.Parse(Console.ReadLine)
-            For i = 1 To repeticion
-                Console.WriteLine("i=" & i)
+        Console.WriteLine("Humano que altura va atener tu escalera")
+        Dim altura = Integer.Parse(Console.ReadLine)
+        For i = 1 To altura
+            For j = 1 To i
+                Console.Write("*")
             Next
-        Catch ex As Exception
-            Console.WriteLine("Humano estupido te pedi un numero")
-        End Try
-
+            Console.WriteLine()
+        Next
         Console.Read()
     End Sub
 End Module
