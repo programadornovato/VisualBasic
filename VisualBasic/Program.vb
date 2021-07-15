33ddc66c658e5b926cdc74f44ad4cf5cba021512
Programador Novato <info@programadornovato.com>
2021-07-15 03:49:22 -0500

    Ejercicio 11.- Identificar un palindromo en Visual Basic.NET [40]⛓️ Video: https://youtu.be/JqLmwpHDzpA

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,17 +6,30 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim perros = New String() {"Monte", "Chupacabras", "Mikey", "Solovino", "El chiquito"}
-        Console.WriteLine("CON FOR")
-        For i = 0 To perros.Length - 1
-            Console.WriteLine("El perro " & i & " es " & perros(i))
-        Next
-        Console.WriteLine("CON FOR EACH")
-        Dim j = 0
-        For Each perro In perros
-            Console.WriteLine("El perro " & j & " es " & perro)
-            j = j + 1
-        Next
+        Console.WriteLine("Humano ingresa un palindromo")
+        Dim texto = Console.ReadLine()
+        Dim textoCopia = texto
+        If texto IsNot Nothing Then
+            texto = texto.ToLower()
+            texto = texto.Replace(" ", "")
+            Dim longitud = texto.Length
+            Dim igual = True
+            Dim cont = 0
+            For indice = longitud - 1 To 0 Step -1
+                If (texto(indice) <> texto(cont)) Then
+                    igual = False
+                    Exit For
+                End If
+                cont = cont + 1
+            Next
+            If igual = True Then
+                Console.WriteLine("Felicidades humano el texto " & textoCopia & " si es palindromo")
+            Else
+                Console.WriteLine("Humano estupido el texto " & textoCopia & " no es palindromo")
+            End If
+        Else
+            Console.WriteLine("humano estupido te pedi que ingresarar un palindo y no escribiste nada")
+        End If
         Console.Read()
     End Sub
 End Module
