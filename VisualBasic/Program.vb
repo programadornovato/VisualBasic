2843c473ace99b88de98b33947b597e2468a43c7
Programador Novato <info@programadornovato.com>
2021-07-21 12:51:16 -0500

    Búsqueda Binaria en Visual Basic.NET[52]⛓️ Video: https://youtu.be/G5WZdCvQ1Pk

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -13,20 +13,30 @@ Module Program
             Console.WriteLine("Humano ingresa el valor del elemento " & i + 1)
             listaNumeros(i) = Integer.Parse(Console.ReadLine())
         Next
-        Dim pos = 0
+        Dim n = listaNumeros.Length
+        Dim inf = 0
+        Dim centro = 0
+        Dim sup = n - 1
         Dim bandera = False
-        While pos < listaNumeros.Length And bandera = False
-            If listaNumeros(pos) = numeroBuscar Then
+        Dim cont = 0
+        While inf <= sup
+            centro = (sup + inf) / 2
+            If listaNumeros(centro) = numeroBuscar Then
                 bandera = True
                 Exit While
+            ElseIf numeroBuscar < listaNumeros(centro) Then
+                sup = centro - 1
+            ElseIf numeroBuscar > listaNumeros(centro) Then
+                inf = centro + 1
             End If
-            pos = pos + 1
+            cont = cont + 1
         End While
         If bandera = True Then
-            Console.WriteLine("Humano el valor " & numeroBuscar & " estaba en la posicion " & pos + 1)
+            Console.WriteLine("El numero " & numeroBuscar & " se encuentra en la posicion " & centro + 1)
         Else
-            Console.WriteLine("Humano estupido el numero " & numeroBuscar & " no esta en la lista")
+            Console.WriteLine("Humano estupi el numero no se encuentra en la lista")
         End If
+        Console.WriteLine("Vueltas = " & cont)
         Console.Read()
     End Sub
 End Module
