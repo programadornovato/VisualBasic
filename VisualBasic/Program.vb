e48617253fcf836a3d4e12aa3ffc2f3278d7bdd6
Programador Novato <info@programadornovato.com>
2021-07-21 14:54:15 -0500

    Matriz en Visual Basic.NET[53]⛓️ Video: https://youtu.be/sUicVZtC-d0

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,37 +6,31 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Console.WriteLine("Humano ingresa el valor objetivo")
-        Dim numeroBuscar = Integer.Parse(Console.ReadLine())
-        Dim listaNumeros = New Integer(6) {}
-        For i = 0 To listaNumeros.Length - 1
-            Console.WriteLine("Humano ingresa el valor del elemento " & i + 1)
-            listaNumeros(i) = Integer.Parse(Console.ReadLine())
-        Next
-        Dim n = listaNumeros.Length
-        Dim inf = 0
-        Dim centro = 0
-        Dim sup = n - 1
-        Dim bandera = False
-        Dim cont = 0
-        While inf <= sup
-            centro = (sup + inf) / 2
-            If listaNumeros(centro) = numeroBuscar Then
-                bandera = True
-                Exit While
-            ElseIf numeroBuscar < listaNumeros(centro) Then
-                sup = centro - 1
-            ElseIf numeroBuscar > listaNumeros(centro) Then
-                inf = centro + 1
-            End If
-            cont = cont + 1
-        End While
-        If bandera = True Then
-            Console.WriteLine("El numero " & numeroBuscar & " se encuentra en la posicion " & centro + 1)
-        Else
-            Console.WriteLine("Humano estupi el numero no se encuentra en la lista")
-        End If
-        Console.WriteLine("Vueltas = " & cont)
+        'Dim matriz = New Integer(1, 2) {}
+        'Console.WriteLine("Filas=" & matriz.GetLength(0))
+        'Console.WriteLine("Columnas=" & matriz.GetLength(1))
+        'Console.WriteLine("Humano ingresa el tamaño de las filas de tu matriz")
+        'Dim filas = Integer.Parse(Console.ReadLine())
+        'Console.WriteLine("Humano ingresa el tamaño de las columnas de tu matriz")
+        'Dim columnas = Integer.Parse(Console.ReadLine())
+        'Dim matriz = New Integer(filas - 1, columnas - 1) {}
+        'For i = 0 To filas - 1
+        '    For j = 0 To columnas - 1
+        '        Console.WriteLine("matriz(" & i + 1 & "," & j + 1 & ")")
+        '        matriz(i, j) = Integer.Parse(Console.ReadLine())
+        '    Next
+        'Next
+        'Console.WriteLine("Humano aqui esta tu pinche matriz")
+        'For i = 0 To filas - 1
+        '    For j = 0 To columnas - 1
+        '        Console.Write(matriz(i, j) & ",")
+        '    Next
+        '    Console.WriteLine()
+        'Next
+        Dim matriz = New Integer(0, 1, 2) {}
+        Console.WriteLine("x=" & matriz.GetLength(0))
+        Console.WriteLine("y=" & matriz.GetLength(1))
+        Console.WriteLine("z=" & matriz.GetLength(2))
         Console.Read()
     End Sub
 End Module
