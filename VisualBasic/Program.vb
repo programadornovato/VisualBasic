db6e76e5dfb0a81911350657338c9dfb155f948e
Programador Novato <info@programadornovato.com>
2021-07-22 21:38:00 -0500

    Matriz de String en Visual Basic.NET[54]⛓️ Video: https://youtu.be/f6R1ewKdBys

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,31 +6,24 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        'Dim matriz = New Integer(1, 2) {}
-        'Console.WriteLine("Filas=" & matriz.GetLength(0))
-        'Console.WriteLine("Columnas=" & matriz.GetLength(1))
-        'Console.WriteLine("Humano ingresa el tamaño de las filas de tu matriz")
-        'Dim filas = Integer.Parse(Console.ReadLine())
-        'Console.WriteLine("Humano ingresa el tamaño de las columnas de tu matriz")
-        'Dim columnas = Integer.Parse(Console.ReadLine())
-        'Dim matriz = New Integer(filas - 1, columnas - 1) {}
-        'For i = 0 To filas - 1
-        '    For j = 0 To columnas - 1
-        '        Console.WriteLine("matriz(" & i + 1 & "," & j + 1 & ")")
-        '        matriz(i, j) = Integer.Parse(Console.ReadLine())
-        '    Next
-        'Next
-        'Console.WriteLine("Humano aqui esta tu pinche matriz")
-        'For i = 0 To filas - 1
-        '    For j = 0 To columnas - 1
-        '        Console.Write(matriz(i, j) & ",")
-        '    Next
-        '    Console.WriteLine()
-        'Next
-        Dim matriz = New Integer(0, 1, 2) {}
-        Console.WriteLine("x=" & matriz.GetLength(0))
-        Console.WriteLine("y=" & matriz.GetLength(1))
-        Console.WriteLine("z=" & matriz.GetLength(2))
+        Dim alumnos = New String(6, 1) {}
+        For i = 0 To alumnos.GetLength(0) - 1
+            Console.WriteLine("Humano ingresa el NOMBRE del alumno " & i + 1)
+            alumnos(i, 0) = Console.ReadLine()
+            Console.WriteLine("Humano ingresa el SALON del alumno " & i + 1)
+            alumnos(i, 1) = Console.ReadLine()
+        Next
+        Console.WriteLine("Humano ingresa el SALON de los alumnos que quieres visualizar")
+        Dim salon = Console.ReadLine()
+        For i = 0 To alumnos.GetLength(0) - 1
+            If salon = alumnos(i, 1) Then
+                'Console.Write(alumnos(i, 0) & " | " & alumnos(i, 1))
+                For j = 0 To alumnos.GetLength(1) - 1
+                    Console.Write(alumnos(i, j) & " | ")
+                Next
+                Console.WriteLine()
+            End If
+        Next
         Console.Read()
     End Sub
 End Module
