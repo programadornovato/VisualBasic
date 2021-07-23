bbcdfc422d87fdf55619b960b1ba763121c797a7
Programador Novato <info@programadornovato.com>
2021-07-23 15:20:24 -0500

    Demostrar que una matriz es simétrica en Visual Basic.NET[56]⛓️ Video: https://youtu.be/mxWUYzM3EAo

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,34 +6,38 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim matrizO = New Integer(2, 2) {}
-        Dim matrizT = New Integer(2, 2) {}
-        For i = 0 To 2
-            For j = 0 To 2
-                Console.WriteLine("humano ingresa el valor de la matriz original (" & i + 1 & "," & j + 1 & ") ")
-                matrizO(i, j) = Integer.Parse(Console.ReadLine())
+        Console.WriteLine("Humano cuantas filas tiene tu matriz")
+        Dim fil = Integer.Parse(Console.ReadLine())
+        Console.WriteLine("Humano cuantas columnas tiene tu matriz")
+        Dim col = Integer.Parse(Console.ReadLine())
+        If fil = col Then
+            Dim matriz = New Integer(fil - 1, col - 1) {}
+            For i = 0 To fil - 1
+                For j = 0 To col - 1
+                    Console.WriteLine("Matriz(" & i + 1 & "," & j + 1 & ")")
+                    matriz(i, j) = Integer.Parse(Console.ReadLine())
+                Next
             Next
-        Next
-        For i = 0 To 2
-            For j = 0 To 2
-                matrizT(i, j) = matrizO(j, i)
+            Dim esSimetrica = True
+            For i = 0 To fil - 1
+                For j = 0 To col - 1
+                    If matriz(i, j) <> matriz(j, i) Then
+                        esSimetrica = False
+                        Exit For
+                    End If
+                Next
+                If esSimetrica = False Then
+                    Exit For
+                End If
             Next
-        Next
-        Console.WriteLine("Humano aqui esta tu pinche matriz original")
-        For i = 0 To 2
-            For j = 0 To 2
-                Console.Write(matrizO(i, j) & " | ")
-            Next
-            Console.WriteLine()
-        Next
-        Console.WriteLine()
-        Console.WriteLine("Humano aqui esta tu pinche matriz transpuesta")
-        For i = 0 To 2
-            For j = 0 To 2
-                Console.Write(matrizT(i, j) & " | ")
-            Next
-            Console.WriteLine()
-        Next
+            If esSimetrica = True Then
+                Console.WriteLine("Felicidades humano tu matriz si es simetrica")
+            Else
+                Console.WriteLine("Humano estupido tu matriz no es simetrica")
+            End If
+        Else
+            Console.WriteLine("Humano las filas y las columnas deben ser iguales tambien")
+        End If
         Console.Read()
     End Sub
 End Module
