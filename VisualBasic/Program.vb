9c729163a0b0a4879cfca09de326fcbd26e392a8
Programador Novato <info@programadornovato.com>
2021-07-26 17:25:21 -0500

    Métodos y Atributos Públicos y Privados en Visual Basic.NET[61]⛓️ Video: https://youtu.be/PsCIfy5BDFU

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -8,39 +8,51 @@ Module Program
     Public Sub Main(args As String())
         Console.WriteLine("Vocho")
         Dim vocho1 = New Auto()
-        vocho1.color = "Naranja"
-        vocho1.modelo = 1980
-        vocho1.marca = "Vocho"
-        Console.WriteLine(vocho1.color)
-        Console.WriteLine(vocho1.modelo)
-        Console.WriteLine(vocho1.marca)
-        vocho1.enciende()
-        vocho1.acelera()
-        vocho1.frena()
+        vocho1.meterLlave("123456")
+        vocho1.mando("enciende")
         Console.WriteLine("Beatle")
         Dim beattle = New Auto()
-        beattle.color = "Gris"
-        beattle.modelo = 2010
-        beattle.marca = "Beattle"
-        Console.WriteLine(beattle.color)
-        Console.WriteLine(beattle.modelo)
-        Console.WriteLine(beattle.marca)
-        beattle.enciende()
-        beattle.acelera()
-        beattle.frena()
+        beattle.meterLlave("123456")
+        beattle.mando("enciende")
+        beattle.mando("acelera")
         Console.Read()
     End Sub
     Class Auto
-        Public color = ""
-        Public modelo = 0
-        Public marca = ""
-        Public Sub enciende()
+        Private color = ""
+        Private modelo = 0
+        Private marca = ""
+        Private acceso = False
+        Public Sub meterLlave(llave As String)
+            If llave = "123456" Then
+                acceso = True
+                Console.WriteLine("Llave correcta")
+            Else
+                acceso = False
+                Console.WriteLine("Llave incorrecta")
+            End If
+        End Sub
+        Public Sub mando(accion As String)
+            If acceso = True Then
+                If accion = "enciende" Then
+                    enciende()
+                End If
+                If accion = "acelera" Then
+                    acelera()
+                End If
+                If accion = "frena" Then
+                    frena()
+                End If
+            Else
+                Console.WriteLine("No se puede realizar la accion " & accion & " porque el acceso es falso")
+            End If
+        End Sub
+        Private Sub enciende()
             Console.WriteLine("El auto ha encendido run run")
         End Sub
-        Public Sub acelera()
+        Private Sub acelera()
             Console.WriteLine("El auto acelera")
         End Sub
-        Public Sub frena()
+        Private Sub frena()
             Console.WriteLine("El auto frena")
         End Sub
     End Class
