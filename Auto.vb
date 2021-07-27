d19f36abf43e75232b232296947d1291c5e84336
Programador Novato <info@programadornovato.com>
2021-07-26 19:10:52 -0500

    Llamar nuestras clases desde otros archivos en Visual Basic.NET[62]⛓️ Video: https://youtu.be/ThWVXeN79i0

---
+++ Auto.vb
@@ -0,0 +1,2 @@
+﻿Imports Microsoft.VisualBasic
+
---
+++ VisualBasic/Auto.vb
@@ -0,0 +1,39 @@
+﻿Class Auto
+    Private color = ""
+    Private modelo = 0
+    Private marca = ""
+    Private acceso = False
+    Public Sub meterLlave(llave As String)
+        If llave = "123456" Then
+            acceso = True
+            Console.WriteLine("Llave correcta")
+        Else
+            acceso = False
+            Console.WriteLine("Llave incorrecta")
+        End If
+    End Sub
+    Public Sub mando(accion As String)
+        If acceso = True Then
+            If accion = "enciende" Then
+                enciende()
+            End If
+            If accion = "acelera" Then
+                acelera()
+            End If
+            If accion = "frena" Then
+                frena()
+            End If
+        Else
+            Console.WriteLine("No se puede realizar la accion " & accion & " porque el acceso es falso")
+        End If
+    End Sub
+    Private Sub enciende()
+        Console.WriteLine("El auto ha encendido run run")
+    End Sub
+    Private Sub acelera()
+        Console.WriteLine("El auto acelera")
+    End Sub
+    Private Sub frena()
+        Console.WriteLine("El auto frena")
+    End Sub
+End Class
