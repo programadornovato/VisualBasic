de3b5f9349047865b26f02022e8038aec66233af
Programador Novato <info@programadornovato.com>
2021-07-28 04:51:36 -0500

    POO Sobrecarga de métodos en Visual Basic.NET [65]⛓️ video: https://youtu.be/t44aEfKR9Sg

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,11 +6,10 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim p1 = New Persona("Juan", 1)
-        'p1.inicializar("Juan", 1)
-        p1.mostrar()
-        Dim p2 = New Persona("Pepe", 2)
-        p2.mostrar()
+        Dim v1 = New Vehiculo("Nissan", "2010")
+        v1.acelera()
+        Dim v2 = New Vehiculo("sku654HH")
+        v2.acelera(20)
         Console.Read()
     End Sub
 End Module
---
+++ VisualBasic/Vehiculo.vb
@@ -0,0 +1,28 @@
+﻿Public Class Vehiculo
+    Private marca As String
+    Private modelo As String
+    Private sku As String
+    Public Sub New(_marca As String, _modelo As String)
+        marca = _marca
+        modelo = _modelo
+    End Sub
+    Public Sub New(_sku As String)
+        sku = _sku
+    End Sub
+    Public Sub acelera()
+        If marca IsNot Nothing Then
+            Console.WriteLine("El vehiculo " & marca & " " & modelo & " esta acelerando")
+        Else
+            Console.WriteLine("El vehiculo " & sku & " esta acelerando")
+        End If
+
+    End Sub
+    Public Sub acelera(kmh As Integer)
+        If marca IsNot Nothing Then
+            Console.WriteLine("El vehiculo " & marca & " " & modelo & " esta acelerando a " & kmh & "km/h")
+        Else
+            Console.WriteLine("El vehiculo " & sku & " esta acelerando a " & kmh & "km/h")
+        End If
+
+    End Sub
+End Class
