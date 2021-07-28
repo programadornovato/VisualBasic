05817a682af7dbb5a01d78634b276254f56c56ce
Programador Novato <info@programadornovato.com>
2021-07-28 18:42:34 -0500

    Espacio de nombres (namespace) en Visual Basic.NET [66]⛓️ Video: https://youtu.be/M6GUVbRzKAk

---
+++ VisualBasic/Algebra.vb
@@ -0,0 +1,13 @@
+﻿Namespace Algebra
+    Public Class Matematicas
+        Private n1 As Integer
+        Private n2 As Integer
+        Public Sub New(_n1 As Integer, _n2 As Integer)
+            n1 = _n1
+            n2 = _n2
+        End Sub
+        Public Function suma()
+            Return n1 + n2
+        End Function
+    End Class
+End Namespace
--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,10 +6,10 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim v1 = New Vehiculo("Nissan", "2010")
-        v1.acelera()
-        Dim v2 = New Vehiculo("sku654HH")
-        v2.acelera(20)
+        Dim mat = New Trigonometria.Matematicas(5, 6)
+        Console.WriteLine(mat.suma())
+        Dim matAlgebra = New Algebra.Matematicas(5, 6)
+        Console.WriteLine(mat.suma())
         Console.Read()
     End Sub
 End Module
