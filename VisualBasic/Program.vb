5ba78798cabed85dee4b75cea99d31a893dce277
Programador Novato <info@programadornovato.com>
2021-07-30 13:45:56 -0500

    Ejercicio 20.- (Arreglo de objetos) Lista de alumnos y calificaciones Visual Basic.NET [71]⛓️ Video: https://youtu.be/lm4nTMXzRt4

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,10 +6,31 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        Dim p = New Personas("Juan", "Perez", 30)
-        Console.WriteLine("Nombre=" & p.getNombre & " Apellido=" & p.getApellido & " Edad=" & p.getEdad)
-        Dim e = New Estudiantes("Pepe", "Perez", 20, "PPP333", 9.5, "A")
-        Console.WriteLine("Nombre=" & e.getNombre & " Apellido=" & e.getApellido & " Edad=" & e.getEdad & " Codigo estudiante=" & e.getCodigoEstudiante & " Calificacion=" & e.getCalificacion & " Salon=" & e.getSalon)
+        Console.WriteLine("Humano ingresa la cantidad de alumnos")
+        Dim cantidadAlumnos = Integer.Parse(Console.ReadLine())
+        Dim a = New Alumnos(cantidadAlumnos - 1) {}
+        For i = 0 To cantidadAlumnos - 1
+            Console.WriteLine("Humano ingresa el NOMBRE del alumno " & (i + 1))
+            Dim nombre = Console.ReadLine()
+            Console.WriteLine("Humano ingresa el AULA del alumno " & (i + 1))
+            Dim aula = Console.ReadLine()(0)
+            Console.WriteLine("Humano ingresa el CALIFICACION del alumno " & (i + 1))
+            Dim cal = Double.Parse(Console.ReadLine())
+            a(i) = New Alumnos(nombre, aula, cal)
+        Next
+        Dim suma = 0.0
+        Dim promedio = 0.0
+        Dim cantidadAlumnosSalon = 0
+        Console.WriteLine("Humano ingresa el salon del que quieres obtener el promedio")
+        Dim salon = Console.ReadLine()(0)
+        For i = 0 To cantidadAlumnos - 1
+            If salon = a(i).getAula Then
+                suma = suma + a(i).getCalificacion
+                cantidadAlumnosSalon = cantidadAlumnosSalon + 1
+            End If
+        Next
+        promedio = suma / cantidadAlumnosSalon
+        Console.WriteLine("El promedio del salon " & salon & " es " & promedio)
         Console.Read()
     End Sub
 End Module
---
+++ VisualBasic/alumnos.vb
@@ -0,0 +1,25 @@
+﻿Public Class Alumnos
+    Private nombre As String
+    Private aula As Char
+    Private calificacion As Double
+    Public Sub New(_nombre As String, _aula As Char, _calificacion As Double)
+        nombre = _nombre
+        aula = _aula
+        If _calificacion > 10 Then
+            calificacion = 10
+        ElseIf _calificacion < 0 Then
+            calificacion = 0
+        Else
+            calificacion = _calificacion
+        End If
+    End Sub
+    Function getNombre()
+        Return nombre
+    End Function
+    Function getCalificacion()
+        Return calificacion
+    End Function
+    Function getAula()
+        Return aula
+    End Function
+End Class
