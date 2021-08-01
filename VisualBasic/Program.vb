7afcdec1c3b37c0672eadc738fc8752d32440c31
Programador Novato <info@programadornovato.com>
2021-08-01 03:31:59 -0500

    Arreglos dinámicos con ArrayList en Visual Basic.NET [74]⛓️ Video: https://youtu.be/kicWOd7fCXQ

--- VisualBasic/Program.vb
+++ VisualBasic/Program.vb
@@ -6,14 +6,35 @@ Imports System
 Module Program
     'Este codigo escribe un texto en amarillo con fondo azul
     Public Sub Main(args As String())
-        'Dim a = New Automotores()
-        'a.llantas()
-        Dim m = New Motocicletas()
-        m.llantas()
-        Dim a = New Autos()
-        a.llantas()
-        Dim c = New Camiones()
-        c.llantas()
+        'listaNormal()
+        listaDinamica()
         Console.Read()
+    End Sub
+    Public Sub listaNormal()
+        Console.WriteLine("Humano por favor ingresa la cantidad de elementos")
+        Dim cantidad = Integer.Parse(Console.ReadLine())
+        Dim lista = New String(cantidad - 1) {}
+        For i = 0 To cantidad - 1
+            Console.WriteLine("Humano por favor ingresa el elemnto de la lista " & i + 1)
+            lista(i) = Console.ReadLine()
+        Next
+        For Each elemento In lista
+            Console.WriteLine(elemento)
+        Next
+    End Sub
+    Public Sub listaDinamica()
+        Dim lista = New ArrayList()
+        Dim respuesta As Char
+        Dim i = 0
+        Do
+            Console.WriteLine("Humano por favor ingresa el elemnto de la lista " & i + 1)
+            lista.Add(Console.ReadLine())
+            Console.WriteLine("Humano, quieres ingresar otro elemnto? s/n")
+            respuesta = Console.ReadLine()(0)
+            i = i + 1
+        Loop While (respuesta = "s" Or respuesta = "S")
+        For Each elemento In lista
+            Console.WriteLine(elemento)
+        Next
     End Sub
 End Module
